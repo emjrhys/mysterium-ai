de2640ba65f3577ae8fd4f5bf244ef0310ed2d0d
Sophie Saito <sophia.lafferty@gmail.com>
2023-09-08 12:22:02 -0400

    structure homepage, add description (attempt two)

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import PlayView from '@/views/PlayView.vue';
+import HomeView from '@/views/HomeView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,8 +8,13 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: PlayView,
+      component: HomeView,
     },
+    {
+      path: '/play',
+      name: 'play',
+      component: PlayView,
+    }
   ],
 });
 
