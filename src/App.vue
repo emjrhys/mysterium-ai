3e901cd943915a92a148d2543404f4b5ae26b7f9
Sophie Saito <sophia.lafferty@gmail.com>
2023-09-08 12:04:04 -0400

    structure homepage, add game description

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <template>
-  <header>
-    <HowToPlayDialog />
-  </header>
-
   <RouterView />
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router';
-import HowToPlayDialog from '@/components/HowToPlayDialog.vue';
 </script>
 
 <style scoped>
--- src/components/HowToPlayDialog.vue
+++ src/components/HowToPlayDialog.vue
@@ -12,9 +12,13 @@
       </v-btn>
     </template>
 
-    <v-card>
+    <v-card id="how-to-play">
       <v-card-text>
-        Here's how to play the game!
+        You have been invited to Murkbrook Manor for a séance. 
+        As the clock strikes five, you make contact with a spirit who has been cursed to haunt the manor until their murderer is brought to justice. 
+        The spirit needs your help. Find their murderer, the room in which the murder occurred, and the murder weapon. 
+        At the top of each hour, the spirit will send you a clue: a memory in the form of a spectral image. 
+        BEWARE! You must correctly interpret the spectral images before the clock strikes twelve, or the spirit will haunt Murkbrook Manor for eternity.
       </v-card-text>
       <v-card-actions>
         <v-btn color="teal-darken-4" block @click="active = false">Close</v-btn>
@@ -28,3 +32,10 @@ import { ref } from 'vue';
 
 const active = ref(false);
 </script>
+
+<style scoped>
+
+#how-to-play {
+  width: 50%;
+}
+</style>
