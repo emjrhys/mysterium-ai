f9d15aea1deec0b7919f78c034e89dd42ffb2da2
Emma Lafferty <emjrhys@gmail.com>
2023-09-08 09:40:40 -0700

    cleanup HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,20 +6,14 @@ function goToPlayView() {
 
 <template>
   <main>
-    <header>
-      <h1>Welcome to Murkbrook Manor</h1>
-    </header>
-    <body>
-      <v-text>You have been invited to Murkbrook Manor for a séance. 
-        As the clock strikes five, you make contact with a spirit who has been cursed to haunt the manor until their murderer is brought to justice. 
-        The spirit needs your help. Find their murderer, the room in which the murder occurred, and the murder weapon. 
-        At the top of each hour, the spirit will send you a clue: a memory in the form of a spectral image. 
-        BEWARE! You must correctly interpret the spectral images before the clock strikes twelve, or the spirit will haunt Murkbrook Manor for eternity.</v-text>
-    </body>
-    <div>
-    <a href="/play">
-      <v-btn color="teal-darken-4" @click="goToPlayView">I'm ready to play!</v-btn>
-    </a>
-  </div>
+    <h1>Welcome to Murkbrook Manor</h1>
+    <div class="mb-4">
+      <p>You have been invited to Murkbrook Manor for a séance.</p>
+      <p>As the clock strikes five, you make contact with a spirit who has been cursed to haunt the manor until their murderer is brought to justice.</p>
+      <p>The spirit needs your help. Find their murderer, the room in which the murder occurred, and the murder weapon.</p>
+      <p>At the top of each hour, the spirit will send you a clue: a memory in the form of a spectral image.</p>
+      <p>BEWARE! You must correctly interpret the spectral images before the clock strikes twelve, or the spirit will haunt Murkbrook Manor for eternity.</p>
+    </div>
+    <v-btn color="teal-darken-4" @click="goToPlayView">I'm ready to play!</v-btn>
   </main>
 </template>
