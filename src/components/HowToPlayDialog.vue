2155515740b7ef3bcde3043d87574a8ee631700d
Sophie Saito <sophia.lafferty@gmail.com>
2023-09-11 11:00:15 -0400

    update how to play instructions

--- src/components/HowToPlayDialog.vue
+++ src/components/HowToPlayDialog.vue
@@ -14,11 +14,11 @@
 
     <v-card>
       <v-card-text>
-        You have been invited to Murkbrook Manor for a séance. 
-        As the clock strikes five, you make contact with a spirit who has been cursed to haunt the manor until their murderer is brought to justice. 
-        The spirit needs your help. Find their murderer, the room in which the murder occurred, and the murder weapon. 
-        At the top of each hour, the spirit will send you a clue: a memory in the form of a spectral image. 
-        BEWARE! You must correctly interpret the spectral images before the clock strikes twelve, or the spirit will haunt Murkbrook Manor for eternity.
+        Use the spectral images to guide you in the right direction.
+        First, click on the person you think is the murderer.
+        When you guess the murderer correctly, click on the room where you think the murder occurred. 
+        When you guess the room correctly, click on the object you think was the murder weapon.
+        If you guess all three correctly before midnight (i.e., within seven guesses), you win!
       </v-card-text>
       <v-card-actions>
         <v-btn color="teal-darken-4" block @click="active = false">Close</v-btn>
